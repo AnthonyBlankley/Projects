ab4b12e9bff059a2826d73d746e7d0ef34753b57
AnthonyBlankley <Anthony.Blankley@aol.com>
2023-08-12 11:19:54 -0400

    modified:   funcInterpolate.swift

--- Swift/funcInterpolate.swift
+++ Swift/funcInterpolate.swift
@@ -1,12 +1,18 @@
+/*create function
+say learning Swift
+*/
+
 func learnSwift (){
 print("Learning Swift")
 }
 
 learnSwift()
 
-
-var anthonyAge: Int
-
+var anthonyAge: Int /*declare var anthonyAge 
+					Holds int type of soon to be age
+					*/
 anthonyAge = 35
+
+//Interpolate variable with string
 
 print("Anthony's age will be \(anthonyAge) soon")
