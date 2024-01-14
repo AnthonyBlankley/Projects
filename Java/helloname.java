32f1469d5aed56bb65b88edb0018195784250e9d
AnthonyBlankley <Anthony.Blankley@aol.com>
2024-01-14 17:55:19 -0500

    modified:   Java/helloname.java

--- Java/helloname.java
+++ Java/helloname.java
@@ -1,14 +1,18 @@
-class helloname{
+   import java.util.Scanner;
+
+   class helloname{
 	
    public static void main(String [] args){
    
-   String name = "Anthony";
-
+   System.out.print("Hi, ");
+   System.out.println("What is Your name? ");
+   Scanner obj = new Scanner(System.in);
+   String name = obj.nextLine();
    System.out.println("Hello, " + name);
    System.out.println("How are you?");
-		
-		
-		
+   Scanner obj2 = new Scanner(System.in);
+   String mood = obj2.nextLine();
+   System.out.println("Pleased to hear that. Have a great day!!!");
 	}
 	
 	
