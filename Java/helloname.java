2958a90776cee158ee2677895504737a08d2fe3f
AnthonyBlankley <Anthony.Blankley@aol.com>
2024-01-20 00:47:02 -0500

    modified: Java/helloname.java

--- Java/helloname.java
+++ Java/helloname.java
@@ -7,12 +7,20 @@
    System.out.print("Hi, ");
    System.out.println("What is Your name? ");
    Scanner obj = new Scanner(System.in);
-   String name = obj.nextLine();
+   String name = obj.next();
    System.out.println("Hello, " + name);
    System.out.println("How are you?");
    Scanner obj2 = new Scanner(System.in);
-   String mood = obj2.nextLine();
-   System.out.println("Pleased to hear that. Have a great day!!!");
+   String mood = obj2.next();
+   System.out.println(mood);
+   System.out.println("I see, you are " + mood + ".");
+   String awesome = "good";
+   if (mood.equals("good")) {
+      System.out.println("You're the best");
+   }
+   else{
+      System.out.println("Hope your day gets better");
+   }
 	}
 	
 	
