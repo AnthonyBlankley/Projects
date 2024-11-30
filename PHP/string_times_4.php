2bf48f1262c9d98351d24ccccfb1c460dbd78554
AnthonyBlankley <Anthony.Blankley@aol.com>
2024-11-30 01:46:00 -0500

    modified

--- PHP/string_times_4.php
+++ PHP/string_times_4.php
@@ -3,7 +3,7 @@
 // declare name as string 
 $name = "Anthony\n";
 
-//print string variable 4 times with str_repat function and printr.
+//print string variable 4 times with str_repeat function and print_r.
 
 print_r(str_repeat($name, 4));
 ?>
