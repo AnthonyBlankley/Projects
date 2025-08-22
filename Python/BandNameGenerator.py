bee72a7364564fb819954c34b2dcf6606129809c
AnthonyBlankley <Anthony.Blankley@aol.com>
2025-08-22 19:28:09 -0400

    fix: update prompt for pet name in Band Name Generator

--- Python/BandNameGenerator.py
+++ Python/BandNameGenerator.py
@@ -1,4 +1,4 @@
 print("Welcome to the Band Name Generator") #print a welcome message saying welcome to the band name generator
 city = input("Which city did you grow up in?\n") #prompt the user for the city they grew up in
-pet = input("What is the name of your pet?\n")  #prompt the user for the name of their pet
+pet = input("What is the name of a pet?\n")  #prompt the user for the name of a pet
 print("Your band name could be " + city + " " + pet) #print the band name suggestion by combining the city and pet name
