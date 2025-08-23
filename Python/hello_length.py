e516f6ba462988cfc66e91a9bd07b7cf537b383e
AnthonyBlankley <Anthony.Blankley@aol.com>
2025-08-22 23:42:36 -0400

    add: hello greeting with len function

---
+++ Python/hello_length.py
@@ -0,0 +1,11 @@
+#Ask user for their name with input function
+name = input("What is your name?\n")
+
+#get length of name
+length = len(name)
+print(length)
+
+#convert name to string
+#Greet user with their name
+str(name)
+print(f"Hello, {name}!")
